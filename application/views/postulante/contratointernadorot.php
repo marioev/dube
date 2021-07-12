a784b52a3fdfffedd36c67d206344f5deb23c511
Mario escobar <escobar_m_98@hotmail.com>
2021-07-12 15:13:56 -0400

    12.07.2021 inicio de internado Rotatorio

--- application/views/postulante/contratointernadorot.php
+++ application/views/postulante/contratointernadorot.php
@@ -36,45 +36,91 @@ $tamanio = "15.5cm";
                         <span class=" text-center text-bold" style="display: block">CONTRATO DE OTORGACION DE BECA PARA INTERNADO ROTATORIO.</span>
                         <br>
                         <br>
-Conste por el presente contrato de otorgacion de Beca para Internado Rotatorio, que celebran por una parte la UNIVERSIDAD MAYOR DE SAN SIMON, con NIT N° 1008485024, domicilio en el Edificio Mariscal Andres de Santa Cruz, ubicado en la Av. Ballivian Esq. Reza de la ciudad de Cochabamba, representada legalmente por el Lic. Juan Alfonso Rios del Prado con C.I. 815338 Cbba. en calidad de Rector, reconocido mediante R.C.U. 21/2016, el Mgr. Jesus Cesar Garcia Zamorano con C.I. N° 3094814 Or. como Director Administrativo y Financiero a.i., designado mediante R.R. 955/2016 y; el Dr. Luis Sergio Gonzales Fernandez, con C.I. 3784290 Cbba, en su Calidad de Director Universitario de Bienestar Estudiantil (DUBE), designado mediante R.R. 879/2016; quienes en adelante se denominaran simplemente como la UMSS, por otra parte, la universitaria ………….nombre…………….., con C.I.  expe….., que en adelante se denominare la BECARIA; quienes celebran y suscriben el presente contrato administrativo de acuerdo a los siguientes terminos y condiciones:
-PRIMERA.- (DE LOS ANTECEDENTES).
-En el marco del D.S. No 0961 de 18 de agosto de 2011 y el Reglamento Especifico de Becas, Infraestructura y Equipamiento con Recursos del Impuesto Directo a los Hidrocarburos (IDH) la Universidad Mayor de San Simon, a traves de Direcci6n Universitaria de Bienestar Estudiantil (DUBE), emitio la Convocatoria publica No. 01/2016 de 01 de abril de 2016 para la admision de becarios de Internado Rotatorio.
-Concluido el plazo establecido en la Convocatoria, y en cumplimiento del procedimiento previsto en el precitado Reglamento Especifico de Becas, Infraestructura y Equipamiento con Recursos del Impuesto Directo a los Hidrocarburos (IDH ) y en el Manual de Procedimientos para la Otorgacion de Becas a los Estudiantes de la UMSS con Recursos del Impuesto a los Hidrocarburos (IDH) Modalidad de Beca de Internado Rotatorio de la Direccion Universitaria de Bienestar Estudiantil DUBE, mediante Resolucion Rectoral No. 561/2017 de 22 de junio de 2017, se aprueba la nomina de postulantes seleccionados por la Comision Tecnica Administrativa IDH y la Comision Estudiantil Administrativa de IDH.
-SEGUNDA.- (DEL OBJETO Y CAUSA).
-El presente contrato tiene por objeto y causa la otorgacion de la Beca de Internado R otatorio con recursos provenientes del IDH y establecer las condiciones de su ejecucion y umplimiento.
-TERCERA.- (MARCO NORMATIVO Y TECNICO).
-El presente contrato tiene como marco normativo y tecnico a las siguientes disposiciones:
-a)	Ley 1178 de AdministraciOn y Control Gubernamental.
-b)	Decreto Supremo 23318-A - Reglamento de la Responsabilidad por la Funci6n Poblica modificado por el D.S. 26237
-c)	Decreto Supremo 0961 de 18 de agosto de 2011
-d)	Estatuto Organic° de la Universidad Mayor de San Sim6n.
-e)	Reglamento Especifico de Becas, Infraestructura y Equipamiento con Recursos del Impuesto Directo a los Hidrocarburos (IDH) la Universidad Mayor de San Simon.
-f)	Resolucion Rectoral No. 561/2017 de 22 de junio de 2017 y Resolucion Rectoral complementaria No. 731/2017 de 11 de agosto de 2017.
-CUARTA.- (MONTO DE LA BECA Y FORMA DE PAGO).
-De conformidad al Reglamento, se fija el monto de la Beca en la suma de Bs.7000,00.- (Siete mil 00/100 Bolivianos), suma de dinero que sera otorgado en dos cuotas.
-• La primera Cuota equivalente al 30% del beneficio total, equivalente a Bs.2.100,00.- (Dos mil cien 00/100 Bolivianos) a la presentacion del Certificado de Aprobacian del Perfil de Tesis.
+                        <div style="text-align: justify; text-justify: inter-word">
+Conste por el presente contrato de otorgacion de Beca para Internado Rotatorio, que
+celebran por una parte la UNIVERSIDAD MAYOR DE SAN SIMON, con NIT N°
+1008485024, domicilio en el Edificio Mariscal Andres de Santa Cruz, ubicado en la
+Av. Ballivian Esq. Reza de la ciudad de Cochabamba, representada legalmente por el
+Lic. Juan Alfonso Rios del Prado con C.I. 815338 Cbba. en calidad de Rector,
+reconocido mediante R.C.U. 21/2016, el Mgr. Jesus Cesar Garcia Zamorano con
+C.I. N° 3094814 Or. como Director Administrativo y Financiero a.i., designado
+mediante R.R. 955/2016 y; el Dr. Luis Sergio Gonzales Fernandez, con C.I. 3784290
+Cbba, en su Calidad de Director Universitario de Bienestar Estudiantil (DUBE),
+designado mediante R.R. 879/2016; quienes en adelante se denominaran simplemente
+como la UMSS, por otra parte, la universitaria ………….nombre…………….., con C.I.
+expe….., que en adelante se denominare la BECARIA; quienes celebran y suscriben el
+presente contrato administrativo de acuerdo a los siguientes terminos y condiciones:
+<br>
+PRIMERA.- (DE LOS ANTECEDENTES).<br>
+En el marco del D.S. No 0961 de 18 de agosto de 2011 y el Reglamento Especifico
+de Becas, Infraestructura y Equipamiento con Recursos del Impuesto Directo a los
+Hidrocarburos (IDH) la Universidad Mayor de San Simon, a traves de Dirección
+Universitaria de Bienestar Estudiantil (DUBE), emitio la Convocatoria publica No.
+01/2016 de 01 de abril de 2016 para la admision de becarios de Internado Rotatorio.
+<br>
+Concluido el plazo establecido en la Convocatoria, y en cumplimiento del
+procedimiento previsto en el precitado Reglamento Especifico de Becas,
+Infraestructura y Equipamiento con Recursos del Impuesto Directo a los Hidrocarburos
+(IDH ) y en el Manual de Procedimientos para la Otorgacion de Becas a los Estudiantes de
+la UMSS con Recursos del Impuesto a los Hidrocarburos (IDH) Modalidad de Beca de
+Internado Rotatorio de la Direccion Universitaria de Bienestar Estudiantil DUBE,
+mediante Resolucion Rectoral No. 561/2017 de 22 de junio de 2017, se aprueba la
+nomina de postulantes seleccionados por la Comision Tecnica Administrativa IDH y la
+Comision Estudiantil Administrativa de IDH.
+<br>
+SEGUNDA.- (DEL OBJETO Y CAUSA).<br>
+El presente contrato tiene por objeto y causa la otorgacion de la Beca de Internado Rotatorio
+con recursos provenientes del IDH y establecer las condiciones de su ejecucion y
+umplimiento.
+<br>
+TERCERA.- (MARCO NORMATIVO Y TECNICO).<br>
+El presente contrato tiene como marco normativo y tecnico a las siguientes disposiciones:<br>
+a)	Ley 1178 de AdministraciOn y Control Gubernamental.<br>
+b)	Decreto Supremo 23318-A - Reglamento de la Responsabilidad por la
+Función Poblica modificado por el D.S. 26237<br>
+c)	Decreto Supremo 0961 de 18 de agosto de 2011<br>
+d)	Estatuto Organic° de la Universidad Mayor de San Simón.<br>
+e)	Reglamento Especifico de Becas, Infraestructura y Equipamiento con
+Recursos del Impuesto Directo a los Hidrocarburos (IDH) la Universidad
+Mayor de San Simon.<br>
+f)	Resolucion Rectoral No. 561/2017 de 22 de junio de 2017 y Resolucion
+Rectoral complementaria No. 731/2017 de 11 de agosto de 2017.<br>
+CUARTA.- (MONTO DE LA BECA Y FORMA DE PAGO).<br>
+De conformidad al Reglamento, se fija el monto de la Beca en la suma de Bs.7000,00.- (Siete
+mil 00/100 Bolivianos), suma de dinero que sera otorgado en dos cuotas.<br>
+• La primera Cuota equivalente al 30% del beneficio total, equivalente a
+Bs.2.100,00.- (Dos mil cien 00/100 Bolivianos) a la presentacion del
+Certificado de Aprobacian del Perfil de Tesis.<br>
 
-QUINTA.- (PLAZO DE DURACION.
-El tiempo maxim° de duracion de la Beca y por ende de Ia vigencia del presente contrato es de doce
-(12) meses, computable desde el 01 de abril del 2017 (R.R No. 731/2017 de 11/08/2017), a cuya finalizacion quedara autornaticamente extinguida cualquier obligacion de la UMSS.
-SEXTA.- (DERECHOS Y OBLIGACIONES).
-	6.1 Son derechos de estudiantes con beca:
-a)	Percibir la beca adjudicada en forma regular durante la gestion y/o period acordado en el presente contrato.
-b)	Presentar reclamo por cualquier situacion irregular.
-c)	Exigir el cumplimiento de las normas y reglamentos establecidos.
-d)	Asumir defensa ante cualquier queja o denuncia.
-e)	Exigir una satisfaccion personal al denunciante cuando la queja o denuncia resultase infundada.
-f)	Solicitar licencia en caso de ausencia justificada ante la Unidad Facultativa o Area que corresponda.
-	6.2 Todo becario tendra las siguientes obligaciones:
-a)	Presentar fotocopia de la Certificacian como Estudiante Internado Rotatorio emitida por la 
-Facultad o Carrera respective donde realiza su internado rotatorio.
-b)	Remitir el formulario de asignacion (a traves del postulante) al Departamento de Personal 
-Academic° DPA al momento de proceder con los pagos programados.
-c)	Respetar y cumplir las normas universitarias y ajustar su conducta al Ordenamiento Juridic° 
-Administrativo Nacional, contenidas en la Ley SAFCO y la Ley Anticorrupcion No 004 Marcelo Quiroga Santa Cruz; al Reglamento Especifico de Becas de la UMSS y al presente contrato.
+QUINTA.- (PLAZO DE DURACION.<br>
+El tiempo maxim° de duracion de la Beca y por ende de Ia vigencia del presente
+contrato es de doce (12) meses, computable desde el 01 de abril del 2017
+(R.R No. 731/2017 de 11/08/2017), a cuya finalizacion quedara autornaticamente
+extinguida cualquier obligacion de la UMSS.<br>
+SEXTA.- (DERECHOS Y OBLIGACIONES).<br>
+	6.1 Son derechos de estudiantes con beca:<br>
+a)	Percibir la beca adjudicada en forma regular durante la gestion y/o period acordado
+en el presente contrato.<br>
+b)	Presentar reclamo por cualquier situacion irregular.<br>
+c)	Exigir el cumplimiento de las normas y reglamentos establecidos.<br>
+d)	Asumir defensa ante cualquier queja o denuncia.<br>
+e)	Exigir una satisfaccion personal al denunciante cuando la queja o denuncia resultase
+infundada.<br>
+f)	Solicitar licencia en caso de ausencia justificada ante la Unidad Facultativa o
+Area que corresponda.<br>
+	6.2 Todo becario tendra las siguientes obligaciones:<br>
+a)	Presentar fotocopia de la Certificacian como Estudiante Internado Rotatorio emitida por la
+Facultad o Carrera respective donde realiza su internado rotatorio.<br>
+b)	Remitir el formulario de asignacion (a traves del postulante) al Departamento de Personal
+Academico DPA al momento de proceder con los pagos programados.
+c)	Respetar y cumplir las normas universitarias y ajustar su conducta al Ordenamiento Juridico 
+Administrativo Nacional, contenidas en la Ley SAFCO y la Ley Anticorrupcion No 004
+Marcelo Quiroga Santa Cruz; al Reglamento Especifico de Becas de la UMSS y al presente contrato.<br>
 d)	Firmar el contrato de otorgaciOn de Beca y entregar una copia del formulario de asignacion de 
-Beca de Internado Rotatorio, dejando una copia del contrato a la DUBE y al Dpto. Personal Academic° para su incorporacion en Ia Planilla.
-e)	Comunicar dentro de las veinticuatro horas, a la instancia acadernica correspondiente y a la DUBE cualquier contingencia de fuerza mayor o caso fortuito que impida el normal desarrollo de sus actividades, justificando el tiempo de impedimento con el debido sustento documentario. 
+Beca de Internado Rotatorio, dejando una copia del contrato a la DUBE y al Dpto.
+Personal Academic° para su incorporacion en Ia Planilla.<br>
+e)	Comunicar dentro de las veinticuatro horas, a la instancia acadernica correspondiente
+y a la DUBE cualquier contingencia de fuerza mayor o caso fortuito que impida el normal desarrollo de sus actividades, justificando el tiempo de impedimento con el debido sustento documentario. 
 6.3 De las causas de revocacion de Ia beca:
 Son causas de revocacion, las previstas en el articulo 40 del Reglamento Especifico de Becas
 Infraestructura y Equipamiento Estudiantil con Recursos del Impuesto Directo a los Hidrocarburos (IDH) de la UMSS.
@@ -85,7 +131,7 @@ Lic. Juan Alfonso Rios del Prado.	Mgr. Jesus Cesar Garcia Zamorano
 RECTOR - UMSS				DIRECTOR ADM. FINANCIERO UMSS
 Dr. Luis Sergio Gonzales Fernandez                           …………………………………………….	
 DIRECTOR - DUBE							BECARIA
-
+                        </div>
                     </td>
                 </tr>
             </table>
